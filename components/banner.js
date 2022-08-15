3b415652229ce043272fbcee8625e6ff084cff0f
BrookFeleke <felekebrook05@gmail.com>
2022-08-15 15:56:15 +0300

    style: add styling for banner component and other base styles :sparkles:

--- components/banner.js
+++ components/banner.js
@@ -5,7 +5,8 @@ const Banner = (props) => {
         <div className={styles.container}>
             <h1 className={styles.title}><span className={styles.title1}>Coffee</span><span className={styles.title2}>Connoisseur</span></h1>
             <p className={styles.subTitle}>Discover you local coffee shops</p>
-            <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+          
+            <div className={styles.buttonWrapper}><button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button></div>
         </div>
     )
 }
---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Main, NextScript, Head } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/IBMPlexSans-Bold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+            <link
+            rel="preload"
+            href="/fonts/IBMPlexSans-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+            <link
+            rel="preload"
+            href="/fonts/IBMPlexSans-SemiBold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+                <link rel="icon" href="/beans.png" />
+        </Head>
+        <body>
+          <Main></Main> 
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
